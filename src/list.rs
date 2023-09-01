e013b85ce053fe3333ae19e36fd11f113b02a4d5
Yota Toyama <raviqqe@gmail.com>
2023-09-01 12:09:40 +1000

    Fix

--- src/list.rs
+++ src/list.rs
@@ -1,13 +1,22 @@
+///  "Leaked" persistent linked list.
 pub struct List<'a, T>(Option<&'a Inner<'a, T>>);
 
 struct Inner<'a, T> {
     value: T,
-    previous: Option<&'a List<T>>,
-    next: Option<&'a List<T>>,
+    previous: Option<&'a List<'a, T>>,
+    next: Option<&'a List<'a, T>>,
 }
 
-impl List {
+impl<'a, T> List<'a, T> {
     pub fn new() -> Self {
         Self(None)
+    }
+
+    pub fn push_front(&self, value: T) -> Self {
+        Self(Some(Inner {
+            value,
+            previous: Some(self),
+            next: None,
+        }))
     }
 }
