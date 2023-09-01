942a9cf168de1942fa12eefdfe65ddcdbcffb4f4
Yota Toyama <raviqqe@gmail.com>
2023-09-01 17:04:03 +1000

    Fix

--- src/dp.rs
+++ src/dp.rs
@@ -11,6 +11,8 @@ pub fn solve(problem: &Problem) -> Option<Problem> {
 
     states.insert(initial);
 
+    let stop_count = problem.routes().flat_map(crate::Route::stops).count();
+
     for stop in problem.routes().flat_map(crate::Route::stops) {
         let mut new_states = HashSet::new();
 
@@ -32,6 +34,8 @@ pub fn solve(problem: &Problem) -> Option<Problem> {
 
     states
         .iter()
+        // TODO Validate routes in a general way.
+        .filter(|routes| routes.iter().map(Vector::len).sum::<usize>() == stop_count)
         .min_by(|one, other| {
             OrderedFloat(calculate_cost(one)).cmp(&OrderedFloat(calculate_cost(other)))
         })
@@ -55,15 +59,36 @@ mod tests {
     use crate::{Location, Route};
 
     #[test]
-    fn nothing() {
+    fn do_nothing() {
         let problem = Problem::new(vec![]);
 
         assert_eq!(solve(&problem), Some(problem));
     }
 
     #[test]
-    fn one_stop() {
+    fn keep_one_stop() {
         let problem = Problem::new(vec![Route::new(vec![Stop::new(Location::new(0.0, 0.0))])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_two_stops() {
+        let problem = Problem::new(vec![Route::new(vec![
+            Stop::new(Location::new(0.0, 0.0)),
+            Stop::new(Location::new(1.0, 0.0)),
+        ])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_three_stops() {
+        let problem = Problem::new(vec![Route::new(vec![
+            Stop::new(Location::new(0.0, 0.0)),
+            Stop::new(Location::new(1.0, 0.0)),
+            Stop::new(Location::new(2.0, 0.0)),
+        ])]);
 
         assert_eq!(solve(&problem), Some(problem));
     }
