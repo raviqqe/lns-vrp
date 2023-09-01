366e595d0ea3b44ec480a121a3e18d0fe8dda6a9
Yota Toyama <raviqqe@gmail.com>
2023-09-01 15:25:16 +1000

    Fix

--- src/dp.rs
+++ src/dp.rs
@@ -1,24 +1,21 @@
-use crate::{Context, Problem, Route};
+use crate::{Problem, Route};
 use core::{
     alloc::Allocator,
     hash::{Hash, Hasher},
 };
 use std::collections::{hash_map::DefaultHasher, HashMap};
 
-pub fn solve<'a, A: Allocator + Hash + Clone + 'a>(
-    context: &Context<A>,
-    problem: &Problem<A>,
-) -> bool {
+pub fn solve<'a, A: Allocator + Hash + Clone + 'a>(problem: &Problem<A>) -> Option<Problem<A>> {
     let mut states = HashMap::<u64, Problem<_>>::new();
-    let locations = problem.routes().iter().flat_map(Route::stops);
+    let _locations = problem.routes().iter().flat_map(Route::stops);
 
     states.insert(hash(problem), problem.clone());
 
-    for state in &states {
+    for _state in &states {
         let _new_states = HashMap::<(), ()>::new();
     }
 
-    !states.is_empty()
+    None
 }
 
 fn hash<A: Allocator + Hash + Clone>(problem: &Problem<A>) -> u64 {
