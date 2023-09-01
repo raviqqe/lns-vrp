cc863553e0905f862961269ad186d90b04b27543
Yota Toyama <raviqqe@gmail.com>
2023-09-01 10:56:02 +1000

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub mod location;
-pub mod route;
-pub mod stop;
-pub mod vehicle;
+mod location;
+mod route;
+mod stop;
+mod vehicle;
+
+pub use self::location::Location;
+pub use self::route::Route;
+pub use self::stop::Stop;
+pub use self::vehicle::Vehicle;
