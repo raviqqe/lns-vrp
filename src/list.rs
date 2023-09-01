5acd0f90a081fb30138b0c65fb138e8b0d506644
Yota Toyama <raviqqe@gmail.com>
2023-09-01 12:05:26 +1000

    Fix

--- src/list.rs
+++ src/list.rs
@@ -1,7 +1,13 @@
-pub struct List<T>(&'static Inner<T>);
+pub struct List<'a, T>(Option<&'a Inner<'a, T>>);
 
-struct Inner<T> {
+struct Inner<'a, T> {
     value: T,
-    previous: Option<&'static List<T>>,
-    next: Option<&'static List<T>>,
+    previous: Option<&'a List<T>>,
+    next: Option<&'a List<T>>,
+}
+
+impl List {
+    pub fn new() -> Self {
+        Self(None)
+    }
 }
