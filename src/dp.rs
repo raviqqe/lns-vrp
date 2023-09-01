77c1532978a4796d571bd8bea1877f3e23502da8
Yota Toyama <raviqqe@gmail.com>
2023-09-01 16:40:55 +0930

    Implement `dp` module (#7)

--- src/dp.rs
+++ src/dp.rs
@@ -1,27 +1,96 @@
-use crate::{Problem, Route};
-use core::{
-    alloc::Allocator,
-    hash::{Hash, Hasher},
-};
-use std::collections::{hash_map::DefaultHasher, HashMap};
+use crate::{utility::calculate_route_cost, Problem, Stop};
+use im::{HashSet, Vector};
+use ordered_float::OrderedFloat;
 
-pub fn solve<'a, A: Allocator + Hash + Clone + 'a>(problem: &Problem<A>) -> Option<Problem<A>> {
-    let mut states = HashMap::<u64, Problem<_>>::new();
-    let _locations = problem.routes().iter().flat_map(Route::stops);
+// TODO Reason no solution.
+pub fn solve(problem: &Problem) -> Option<Problem> {
+    let mut states = HashSet::<Vector<Vector<Stop>>>::new();
+    let initial = problem
+        .routes()
+        .map(|_| Default::default())
+        .collect::<Vector<_>>();
 
-    states.insert(hash(problem), problem.clone());
+    states.insert(initial);
 
-    for _state in &states {
-        let _new_states = HashMap::<(), ()>::new();
+    let stop_count = problem.routes().flat_map(crate::Route::stops).count();
+
+    for stop in problem.routes().flat_map(crate::Route::stops) {
+        let mut new_states = HashSet::new();
+
+        for routes in &states {
+            for (index, stops) in routes.iter().enumerate() {
+                let mut stops = stops.clone();
+                stops.push_back(stop.clone());
+
+                let mut routes = routes.clone();
+                routes.set(index, stops);
+
+                // TODO Validate a route.
+                new_states.insert(routes);
+            }
+        }
+
+        states = new_states;
     }
 
-    None
+    states
+        .iter()
+        // TODO Validate routes in a general way.
+        .filter(|routes| routes.iter().map(Vector::len).sum::<usize>() == stop_count)
+        .min_by(|one, other| {
+            OrderedFloat(calculate_cost(one)).cmp(&OrderedFloat(calculate_cost(other)))
+        })
+        .map(|routes| {
+            Problem::new(
+                routes
+                    .iter()
+                    .map(|stops| crate::Route::new(stops.iter().cloned().collect()))
+                    .collect(),
+            )
+        })
 }
 
-fn hash<A: Allocator + Hash + Clone>(problem: &Problem<A>) -> u64 {
-    let mut hasher = DefaultHasher::new();
+fn calculate_cost(routes: &Vector<Vector<Stop>>) -> f64 {
+    routes.iter().map(calculate_route_cost).sum()
+}
 
-    problem.hash(&mut hasher);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{Location, Route};
 
-    hasher.finish()
+    #[test]
+    fn do_nothing() {
+        let problem = Problem::new(vec![]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_one_stop() {
+        let problem = Problem::new(vec![Route::new(vec![Stop::new(Location::new(0.0, 0.0))])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_two_stops() {
+        let problem = Problem::new(vec![Route::new(vec![
+            Stop::new(Location::new(0.0, 0.0)),
+            Stop::new(Location::new(1.0, 0.0)),
+        ])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_three_stops() {
+        let problem = Problem::new(vec![Route::new(vec![
+            Stop::new(Location::new(0.0, 0.0)),
+            Stop::new(Location::new(1.0, 0.0)),
+            Stop::new(Location::new(2.0, 0.0)),
+        ])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
 }
