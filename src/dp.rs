a4e9720be8c51618c4057f049a87316c72ab3bcc
Yota Toyama <raviqqe@gmail.com>
2023-09-01 14:33:02 +1000

    Fix

--- src/dp.rs
+++ src/dp.rs
@@ -1,6 +1,7 @@
 use crate::{Context, Problem};
 use core::alloc::Allocator;
+use std::collections::HashMap;
 
 pub fn solve<'a, A: Allocator + 'a>(_context: &Context<A>, _problem: &Problem<A>) -> bool {
-    todo!()
+    let mut state = HashMap::new();
 }
