535983c53b0ca74baa03daa6cc05665886d34f21
Yota Toyama <raviqqe@gmail.com>
2023-09-02 23:08:31 +1000

    Fix

--- src/solve.rs
+++ src/solve.rs
@@ -2,4 +2,7 @@ mod branch_and_bound;
 mod dynamic_programming;
 mod solver;
 
-pub use self::{dynamic_programming::DynamicProgrammingSolver, solver::Solver};
+pub use self::{
+    branch_and_bound::BranchAndBoundSolver, dynamic_programming::DynamicProgrammingSolver,
+    solver::Solver,
+};
