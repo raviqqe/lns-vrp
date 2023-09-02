7bcedc57c655345cc484ea1a53919d3a72783d19
Yota Toyama <raviqqe@gmail.com>
2023-09-02 22:46:28 +1000

    Fix

--- src/solve/branch_and_bound.rs
+++ src/solve/branch_and_bound.rs
@@ -27,20 +27,20 @@ impl<C: CostCalculator> Solver for BranchAndBoundSolver<C> {
         for stop in problem.routes().flat_map(Route::stops) {
             let mut new_states = states.clone();
 
-            for (initial_routes, upper_bound) in &states {
-                for (index, stops) in initial_routes.iter().enumerate() {
-                    let new_routes = {
+            for (routes, upper_bound) in &states {
+                for (index, stops) in routes.iter().enumerate() {
+                    let routes = {
                         let mut stops = stops.clone();
                         stops.push_back(stop.clone());
 
-                        let mut routes = initial_routes.clone();
+                        let mut routes = routes.clone();
                         routes.set(index, stops);
                         routes
                     };
-                    let lower_bound = self.cost_calculator.calculate_lower_bound(&new_routes);
+                    let lower_bound = self.cost_calculator.calculate_lower_bound(&routes);
 
-                    if lower_bound < upper_bound {
-                        new_states.insert(routes, cost);
+                    if lower_bound < *upper_bound {
+                        new_states.insert(routes, lower_bound);
                     }
                 }
             }
