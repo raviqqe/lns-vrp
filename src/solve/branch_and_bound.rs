ffa557d776417c7865c335824e929165d1229593
Yota Toyama <raviqqe@gmail.com>
2023-09-02 20:11:32 +1000

    Fix

---
+++ src/solve/branch_and_bound.rs
@@ -0,0 +1,131 @@
+use super::solver::Solver;
+use crate::{cost::CostCalculator, Problem, Route, Stop};
+use im_rc::{HashSet, Vector};
+use ordered_float::OrderedFloat;
+
+pub struct BranchAndBoundSolver<C: CostCalculator> {
+    cost_calculator: C,
+}
+
+impl<C: CostCalculator> BranchAndBoundSolver<C> {
+    pub fn new(cost_calculator: C) -> Self {
+        Self { cost_calculator }
+    }
+}
+
+impl<C: CostCalculator> Solver for BranchAndBoundSolver<C> {
+    fn solve(&self, problem: &Problem) -> Option<Problem> {
+        let mut states = HashSet::<Vector<Vector<Stop>>>::new();
+        let initial = problem
+            .routes()
+            .map(|_| Default::default())
+            .collect::<Vector<_>>();
+
+        states.insert(initial);
+
+        for stop in problem.routes().flat_map(Route::stops) {
+            let mut new_states = states.clone();
+
+            for routes in &states {
+                for (index, stops) in routes.iter().enumerate() {
+                    let mut stops = stops.clone();
+                    stops.push_back(stop.clone());
+
+                    let mut routes = routes.clone();
+                    routes.set(index, stops);
+
+                    if self.cost_calculator.calculate(&routes).is_finite() {
+                        new_states.insert(routes);
+                    }
+                }
+            }
+
+            states = new_states;
+        }
+
+        states
+            .iter()
+            .map(|routes| (routes, self.cost_calculator.calculate(routes)))
+            .min_by(|(_, one), (_, other)| OrderedFloat(*one).cmp(&OrderedFloat(*other)))
+            .map(|(routes, _)| {
+                Problem::new(
+                    routes
+                        .iter()
+                        .map(|stops| Route::new(stops.iter().cloned().collect()))
+                        .collect(),
+                )
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{cost::DeliveryCostCalculator, Location, Route};
+
+    const DISTANCE_COST: f64 = 1.0;
+    const MISSED_DELIVERY_COST: f64 = 1e9;
+    const QUADRATIC_DISTANCE_COST: f64 = 1e-9;
+
+    fn solve(problem: &Problem) -> Option<Problem> {
+        BranchAndBoundSolver::new(DeliveryCostCalculator::new(
+            problem.routes().flat_map(|route| route.stops()).count(),
+            MISSED_DELIVERY_COST,
+            DISTANCE_COST,
+            QUADRATIC_DISTANCE_COST,
+        ))
+        .solve(problem)
+    }
+
+    #[test]
+    fn do_nothing() {
+        let problem = Problem::new(vec![]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_one_stop() {
+        let problem = Problem::new(vec![Route::new(vec![Stop::new(Location::new(0.0, 0.0))])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_two_stops() {
+        let problem = Problem::new(vec![Route::new(vec![
+            Stop::new(Location::new(0.0, 0.0)),
+            Stop::new(Location::new(1.0, 0.0)),
+        ])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn keep_three_stops() {
+        let problem = Problem::new(vec![Route::new(vec![
+            Stop::new(Location::new(0.0, 0.0)),
+            Stop::new(Location::new(1.0, 0.0)),
+            Stop::new(Location::new(2.0, 0.0)),
+        ])]);
+
+        assert_eq!(solve(&problem), Some(problem));
+    }
+
+    #[test]
+    fn even_workload() {
+        let problem = Problem::new(vec![
+            Route::new(vec![
+                Stop::new(Location::new(0.0, 0.0)),
+                Stop::new(Location::new(1.0, 0.0)),
+                Stop::new(Location::new(2.0, 0.0)),
+            ]),
+            Route::new(vec![]),
+        ]);
+
+        assert!(solve(&problem)
+            .unwrap()
+            .routes()
+            .all(|route| route.stops().len() < 3));
+    }
+}
