24339ca7168a540ce9b84bcdeb721892197ea522
Yota Toyama <raviqqe@gmail.com>
2023-09-02 23:21:31 +1000

    Fix

--- benches/main.rs
+++ benches/main.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 use rand::random;
 use vrp::{
     cost::DeliveryCostCalculator,
-    solve::{DynamicProgrammingSolver, Solver},
+    solve::{BranchAndBoundSolver, DynamicProgrammingSolver, Solver},
     Location, Problem, Route, Stop,
 };
 
@@ -21,8 +21,8 @@ fn random_location() -> Location {
     Location::new(random_longitude(), 0.0)
 }
 
-fn delivery(bencher: &mut Bencher) {
-    let problem = Problem::new(
+fn random_problem() -> Problem {
+    Problem::new(
         [Route::new(
             (0..STOP_COUNT)
                 .map(|_| Stop::new(random_location()))
@@ -31,22 +31,35 @@ fn delivery(bencher: &mut Bencher) {
         .into_iter()
         .chain((1..VEHICLE_COUNT).map(|_| Route::new(vec![])))
         .collect(),
-    );
+    )
+}
 
-    let solver = DynamicProgrammingSolver::new(DeliveryCostCalculator::new(
+fn create_cost_calculator(problem: &Problem) -> DeliveryCostCalculator {
+    DeliveryCostCalculator::new(
         problem.routes().flat_map(|route| route.stops()).count(),
         MISSED_DELIVERY_COST,
         DISTANCE_COST,
         QUADRATIC_DISTANCE_COST,
-    ));
+    )
+}
 
-    bencher.iter(|| {
-        solver.solve(&problem);
-    });
+fn dynamic_programming(bencher: &mut Bencher) {
+    let problem = random_problem();
+    let solver = DynamicProgrammingSolver::new(create_cost_calculator(&problem));
+
+    bencher.iter(|| solver.solve(&problem).unwrap());
+}
+
+fn branch_and_bound(bencher: &mut Bencher) {
+    let problem = random_problem();
+    let solver = BranchAndBoundSolver::new(create_cost_calculator(&problem));
+
+    bencher.iter(|| solver.solve(&problem).unwrap());
 }
 
 fn benchmark(criterion: &mut Criterion) {
-    criterion.bench_function("delivery", delivery);
+    criterion.bench_function("dynamic programming", dynamic_programming);
+    criterion.bench_function("branch and bound", branch_and_bound);
 }
 
 criterion_group!(benchmark_group, benchmark);
