c4c33b957fc5911ba32b7c8fabc025038da77e06
Yota Toyama <raviqqe@gmail.com>
2023-09-03 22:24:31 +1000

    Fix

--- src/solve/dynamic_programming.rs
+++ src/solve/dynamic_programming.rs
@@ -129,6 +129,20 @@ mod tests {
     }
 
     #[test]
+    fn optimize_stop_order() {
+        let problem = SimpleProblem::new(
+            vec![Vehicle::new()],
+            vec![
+                Stop::new(Location::new(0.0, 0.0)),
+                Stop::new(Location::new(2.0, 0.0)),
+                Stop::new(Location::new(1.0, 0.0)),
+            ],
+        );
+
+        assert_eq!(solve(&problem), Solution::new(vec![vec![0, 2, 1]]));
+    }
+
+    #[test]
     fn even_workload() {
         let problem = SimpleProblem::new(
             vec![Vehicle::new(), Vehicle::new()],
