df6d9a07c9f343dd55a2c94146f8e5b616633af5
Yota Toyama <raviqqe@gmail.com>
2023-09-03 21:53:07 +1000

    Fix

---
+++ src/solve/ruin_and_recreate.rs
@@ -0,0 +1,134 @@
+use super::solver::Solver;
+use crate::{cost::CostCalculator, hash_map::HashMap, problem::BaseProblem, Solution};
+use bumpalo::Bump;
+use ordered_float::OrderedFloat;
+
+pub struct RuinAndRecreateSolver<C: CostCalculator> {
+    cost_calculator: C,
+}
+
+impl<C: CostCalculator> RuinAndRecreateSolver<C> {
+    pub fn new(cost_calculator: C) -> Self {
+        Self { cost_calculator }
+    }
+}
+
+impl<C: CostCalculator> Solver for RuinAndRecreateSolver<C> {
+    fn solve(&mut self, problem: impl BaseProblem) -> Solution {
+        let allocator = Bump::new();
+        let mut solutions = HashMap::default();
+        let solution = Solution::new({
+            let mut routes = Vec::with_capacity_in(problem.vehicle_count(), &allocator);
+            routes.extend((0..problem.vehicle_count()).map(|_| Vec::new_in(&allocator)));
+            routes
+        });
+        let cost = self.cost_calculator.calculate(&solution);
+        solutions.insert(solution, cost);
+        let mut new_solutions = vec![];
+
+        for stop_index in 0..problem.stop_count() {
+            new_solutions.clear();
+
+            for solution in solutions.keys() {
+                for vehicle_index in 0..solution.routes().len() {
+                    let solution = solution.add_stop(vehicle_index, stop_index);
+                    let cost = self.cost_calculator.calculate(&solution);
+
+                    if cost.is_finite() {
+                        new_solutions.push((solution, cost));
+                    }
+                }
+            }
+
+            solutions.extend(new_solutions.drain(..));
+        }
+
+        let solution = solutions
+            .into_iter()
+            .min_by(|(_, one), (_, other)| OrderedFloat(*one).cmp(&OrderedFloat(*other)))
+            .expect("at least one solution")
+            .0;
+
+        solution.to_global()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        cost::{DeliveryCostCalculator, DistanceCostCalculator},
+        Location, SimpleProblem, Stop, Vehicle,
+    };
+
+    const DISTANCE_COST: f64 = 1.0;
+    const MISSED_DELIVERY_COST: f64 = 1e9;
+
+    fn solve(problem: &SimpleProblem) -> Solution {
+        RuinAndRecreateSolver::new(DeliveryCostCalculator::new(
+            DistanceCostCalculator::new(problem),
+            problem.stops().len(),
+            MISSED_DELIVERY_COST,
+            DISTANCE_COST,
+        ))
+        .solve(problem)
+    }
+
+    #[test]
+    fn do_nothing() {
+        let problem = SimpleProblem::new(vec![Vehicle::new()], vec![]);
+
+        assert_eq!(solve(&problem), Solution::new(vec![vec![]]));
+    }
+
+    #[test]
+    fn keep_one_stop() {
+        let problem = SimpleProblem::new(
+            vec![Vehicle::new()],
+            vec![Stop::new(Location::new(0.0, 0.0))],
+        );
+
+        assert_eq!(solve(&problem), Solution::new(vec![vec![0]]));
+    }
+
+    #[test]
+    fn keep_two_stops() {
+        let problem = SimpleProblem::new(
+            vec![Vehicle::new()],
+            vec![
+                Stop::new(Location::new(0.0, 0.0)),
+                Stop::new(Location::new(1.0, 0.0)),
+            ],
+        );
+
+        assert_eq!(solve(&problem), Solution::new(vec![vec![0, 1]]));
+    }
+
+    #[test]
+    fn keep_three_stops() {
+        let problem = SimpleProblem::new(
+            vec![Vehicle::new()],
+            vec![
+                Stop::new(Location::new(0.0, 0.0)),
+                Stop::new(Location::new(1.0, 0.0)),
+                Stop::new(Location::new(2.0, 0.0)),
+            ],
+        );
+
+        assert_eq!(solve(&problem), Solution::new(vec![vec![0, 1, 2]]));
+    }
+
+    #[test]
+    fn even_workload() {
+        let problem = SimpleProblem::new(
+            vec![Vehicle::new(), Vehicle::new()],
+            vec![
+                Stop::new(Location::new(0.0, 0.0)),
+                Stop::new(Location::new(1.0, 0.0)),
+                Stop::new(Location::new(2.0, 0.0)),
+            ],
+        );
+
+        assert!(solve(&problem).routes().iter().all(|stops| stops.len() < 3));
+    }
+}
