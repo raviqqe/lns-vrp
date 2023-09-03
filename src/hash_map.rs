50336220985bf2bb0a67c687781a3cac6337ff2f
Yota Toyama <raviqqe@gmail.com>
2023-09-03 18:08:06 +1000

    Fix again

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -1,6 +1,6 @@
 use ahash::AHasher;
 use std::hash::BuildHasherDefault;
 
-// We do not use random state just for dterminism in testing.
+// We do not use random state just for determinism in testing.
 // Do not do this on production!
 pub type HashMap<K, V> = std::collections::HashMap<K, V, BuildHasherDefault<AHasher>>;
