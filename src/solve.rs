b13c834cc667de93c4f9c490d17ddf4efbf6d0e6
Yota Toyama <raviqqe@gmail.com>
2023-09-04 16:42:29 +1000

    Fix

--- src/solve.rs
+++ src/solve.rs
@@ -1,9 +1,11 @@
 mod branch_and_bound;
 mod dynamic_programming;
+mod nearest_neighbour;
 mod ruin_and_recreate;
 mod solver;
 
 pub use self::{
     branch_and_bound::BranchAndBoundSolver, dynamic_programming::DynamicProgrammingSolver,
-    ruin_and_recreate::RuinAndRecreateSolver, solver::Solver,
+    nearest_neighbour::NearestNeighbourSolver, ruin_and_recreate::RuinAndRecreateSolver,
+    solver::Solver,
 };
