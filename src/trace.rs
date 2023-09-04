4febbb79a90c009f6c0eed974a6f8f706a80d78a
Yota Toyama <raviqqe@gmail.com>
2023-09-04 21:56:15 +1000

    Fix

--- src/trace.rs
+++ src/trace.rs
@@ -1,7 +1,10 @@
 #[macro_export]
 macro_rules! trace {
-    ($expr:expr) => {
+    ($template:literal) => {
+        trace!($template,);
+    };
+    ($template:literal, $($expr:expr),*) => {
         #[cfg(feature = "trace")]
-        println!("{:?}", $expr);
+        println!($template, $($expr),*);
     };
 }
