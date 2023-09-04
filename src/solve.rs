9de3c5175c8f8263d79fecf1328b93fbcb986621
Yota Toyama <raviqqe@gmail.com>
2023-09-04 16:52:54 +1000

    Remove nearest neighbour

--- src/solve.rs
+++ src/solve.rs
@@ -1,11 +1,9 @@
 mod branch_and_bound;
 mod dynamic_programming;
-mod nearest_neighbour;
 mod ruin_and_recreate;
 mod solver;
 
 pub use self::{
     branch_and_bound::BranchAndBoundSolver, dynamic_programming::DynamicProgrammingSolver,
-    nearest_neighbour::NearestNeighbourSolver, ruin_and_recreate::RuinAndRecreateSolver,
-    solver::Solver,
+    ruin_and_recreate::RuinAndRecreateSolver, solver::Solver,
 };
