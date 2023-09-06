7641038377cc542d3f36892b743b20cf426f25f1
Yota Toyama <raviqqe@gmail.com>
2023-09-06 14:36:01 +1000

    Fix

---
+++ src/route/cached.rs
@@ -0,0 +1,27 @@
+use std::cell::RefMut;
+
+use super::Router;
+use crate::{hash_map::HashMap, Location};
+use geo::GeodesicDistance;
+use ordered_float::OrderedFloat;
+
+#[derive(Debug, Default)]
+pub struct CachedRouter<R: Router> {
+    router: R,
+    cache: RefMut<HashMap<OrderedFloat<f64>, OrderedFloat<f64>>>,
+}
+
+impl<R: Router> CachedRouter<R> {
+    pub const fn new(router: R) -> Self {
+        Self {
+            router,
+            cache: Default::default(),
+        }
+    }
+}
+
+impl<R: Router> Router for &CachedRouter<R> {
+    fn route(&self, start: &Location, end: &Location) -> f64 {
+        start.as_point().geodesic_distance(end.as_point())
+    }
+}
