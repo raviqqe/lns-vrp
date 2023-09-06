381f235d0b5dce74c15e30d45c4ea70aac43c9db
Yota Toyama <raviqqe@gmail.com>
2023-09-06 17:40:35 +1000

    Fix

--- src/problem/base.rs
+++ src/problem/base.rs
@@ -2,10 +2,12 @@ use crate::Location;
 
 pub trait BaseProblem {
     fn vehicle_count(&self) -> usize;
+    // TODO Add a vehicle trait.
     fn vehicle_start_location(&self, index: usize) -> usize;
     fn vehicle_end_location(&self, index: usize) -> usize;
 
     fn stop_count(&self) -> usize;
+    // TODO Add a stop trait.
     fn stop_location(&self, index: usize) -> usize;
 
     fn location_count(&self) -> usize;
