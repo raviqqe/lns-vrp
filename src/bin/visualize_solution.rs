adc3554517d21883bb5760ea8185a5315c7d8171
Yota Toyama <raviqqe@gmail.com>
2023-09-08 16:40:34 +1000

    Fix

--- src/bin/visualize_solution.rs
+++ src/bin/visualize_solution.rs
@@ -3,8 +3,8 @@ use vrp::{SimpleProblem, Solution};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = args().collect::<Vec<_>>();
-    let problem = SimpleProblem::from_json(serde_json::from_str(&read_to_string(&args[0])?)?)?;
-    let solution = Solution::from_json(serde_json::from_str(&read_to_string(&args[1])?)?)?;
+    let problem = SimpleProblem::from_json(serde_json::from_str(&read_to_string(&args[1])?)?)?;
+    let solution = Solution::from_json(serde_json::from_str(&read_to_string(&args[2])?)?)?;
 
     println!("{}", solution.to_geojson(&problem));
 
