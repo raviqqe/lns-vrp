9d74d5937df2af1f640669ace17005da33d97390
Yota Toyama <raviqqe@gmail.com>
2023-10-23 19:04:20 +1100

    Fix

--- app/src/problem.rs
+++ app/src/problem.rs
@@ -1,5 +1,66 @@
-mod basic;
-mod simple;
+use crate::{Location, Stop, Vehicle};
+use alloc::vec::Vec;
+use serde::{Deserialize, Serialize};
 
-pub use basic::BasicProblem;
-pub use simple::SimpleProblem;
+#[derive(Debug, Deserialize, Serialize)]
+pub struct SimpleProblem {
+    vehicles: Vec<Vehicle>,
+    stops: Vec<Stop>,
+    locations: Vec<Location>,
+}
+
+impl SimpleProblem {
+    pub fn new(vehicles: Vec<Vehicle>, stops: Vec<Stop>, locations: Vec<Location>) -> Self {
+        Self {
+            vehicles,
+            stops,
+            locations,
+        }
+    }
+
+    pub fn vehicles(&self) -> &[Vehicle] {
+        &self.vehicles
+    }
+
+    pub fn stops(&self) -> &[Stop] {
+        &self.stops
+    }
+
+    pub fn to_json(&self) -> Result<serde_json::value::Value, serde_json::Error> {
+        serde_json::to_value(self)
+    }
+
+    pub fn from_json(value: serde_json::value::Value) -> Result<Self, serde_json::Error> {
+        serde_json::from_value(value)
+    }
+}
+
+impl core::BasicProblem for &SimpleProblem {
+    fn vehicle_count(&self) -> usize {
+        self.vehicles.len()
+    }
+
+    fn vehicle_start_location(&self, index: usize) -> usize {
+        self.vehicles[index].start_location()
+    }
+
+    fn vehicle_end_location(&self, index: usize) -> usize {
+        self.vehicles[index].end_location()
+    }
+
+    fn stop_count(&self) -> usize {
+        self.stops.len()
+    }
+
+    fn stop_location(&self, index: usize) -> usize {
+        self.stops[index].location()
+    }
+
+    fn location_count(&self) -> usize {
+        self.locations.len()
+    }
+
+    fn location(&self, index: usize) -> &Location {
+        &self.locations[index]
+    }
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,1 +1,14 @@
-pub mod solver;
+#![feature(allocator_api)]
+
+extern crate alloc;
+extern crate core;
+
+mod location;
+mod problem;
+mod solution;
+mod solver;
+
+pub use location::Location;
+pub use problem::BasicProblem;
+pub use solution::Solution;
+pub use solver::Solver;
